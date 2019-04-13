639f822a729c19d3a61c650e6c4d8411fbe32801
Van DeSantis <vandesantis98@gmail.com>
2019-04-13 18:44:32 -0500

    Fix bugs

--- frontend/src/components/UserProfile/UserShowEntry/UserShowEntry.js
+++ frontend/src/components/UserProfile/UserShowEntry/UserShowEntry.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const UserShowEntry = ({ openShowPage, title, genre, watchingStatus, rating, synopsis, fcc }) => {
     return (
-        <tr class='home-list'>
+        <tr className='home-list'>
             <td><p onClick={() => openShowPage(title, genre, rating, synopsis, fcc)} className='title dim'>{title}</p></td>
             <td className='genre'>{genre}</td>
             <td className='watchingStatus'>{watchingStatus}</td>
