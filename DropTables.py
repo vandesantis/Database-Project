54e20f3526567249070a0a5011a7f9dcd6b15699
DylanASwift <49011161+DylanASwift@users.noreply.github.com>
2019-05-01 16:04:34 -0500

    Add files via upload

--- DropTables.py
+++ DropTables.py
@@ -1,62 +1,53 @@
-import mysql.connector
+import sqlite3
 
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "root",
-    database = "tvbase"
-    )
-
-print(mydb)
-
-mycursor = mydb.cursor()
+dbconn = sqlite3.connect("tvbase.db")
 
 # DROP EACH POSSIBLE TABLE
 sql = "DROP TABLE TV_Show"
 try:
-    mycursor.execute(sql)
+    dbconn.execute(sql)
 
 except:
     print("TABLE NOT DROPPED")
     
 sql = "DROP TABLE User"
 try:
-    mycursor.execute(sql)
+    dbconn.execute(sql)
 
 except:
     print("TABLE NOT DROPPED")
     
 sql = "DROP TABLE Listed_Show"
 try:
-    mycursor.execute(sql)
+    dbconn.execute(sql)
 
 except:
     print("TABLE NOT DROPPED")
     
 sql = "DROP TABLE Studio"
 try:
-    mycursor.execute(sql)
+    dbconn.execute(sql)
 
 except:
     print("TABLE NOT DROPPED")
     
 sql = "DROP TABLE Actor"
 try:
-    mycursor.execute(sql)
+    dbconn.execute(sql)
 
 except:
     print("TABLE NOT DROPPED")
     
 sql = "DROP TABLE TV_Show_Actor"
 try:
-    mycursor.execute(sql)
+    dbconn.execute(sql)
 
 except:
     print("TABLE NOT DROPPED")
     
 sql = "DROP TABLE TV_Show_Studio"
 try:
-    mycursor.execute(sql)
+    dbconn.execute(sql)
 
 except:
     print("TABLE NOT DROPPED")
